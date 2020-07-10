b7629f1fc5d72903e7f0b26e4f81c0dad286be5e
aachh <antoni.chmiela@gmail.com>
2020-07-10 16:13:16 +0200

    + file and directory ignoring

--- src/compile.nim
+++ src/compile.nim
@@ -5,35 +5,58 @@ type
   ClickConfig = object
     compiler, outputDir: string
     auxOutput: bool
+    ignoreDirs, ignoreFiles: seq[string]
 
 ## Compile all C files found in the workspace.
 proc compile*(fromFile: bool) =
   var config: ClickConfig = ClickConfig(
     compiler: "clang",
     outputDir: "./build/",
-    auxOutput: true
+    auxOutput: true,
+    ignoreDirs: @[".vscode/"],
+    ignoreFiles: @[]
   )
   if fromFile:
     var stream = newFileStream("./click.yaml")
     load(stream, config)
     stream.close()
+    # TODO: NORMALIZE EVERY PATH HERE
+  var ignoreDirs: seq[string] = @[]
+  var ignoreFiles: seq[string] = @[]
+  
+  # Normalize directory names
+  for dir in config.ignoreDirs:
+    ignoreDirs.add(dir.expandFilename())
+  config.ignoreDirs = ignoreDirs
+
+  # Normalize filenames
+  for file in config.ignoreFiles:
+    ignoreFiles.add(file.expandFilename())
+  config.ignoreFiles = ignoreFiles
+
+
+
   var inputFiles: seq[string] = @[]
   var command = "echo You shouldn't see this! Something went wrong!\nStart an issue (https://github.com/aachh/click/issues) if you're seeing this. && exit 1"
+
   for file in walkDirRec("./"):
+    if config.ignoreFiles.contains(file.expandFilename()):                continue # First check if we should ignore this file
+    if config.ignoreDirs.contains(splitPath(file).head.expandFilename()): continue # Then check if we should ignore this directory
+
     if file[^2..^1] == ".c":
+      echo(file)
       inputFiles.add(file)
 
   if inputFiles.len() == 0:
     echo("Hmm… are you sure this is a C project? I couldn't find any C files and therefore wasn't able compile them. 😕")
     quit(1)
   
-  
-  var outputFile = config.outputDir / getCurrentDir().lastPathPart() # Not division; path joining. Pretty sick.
+  let outputFile = config.outputDir / getCurrentDir().lastPathPart() # Not division; path joining. Pretty sick.
   
   command = config.compiler & " " & inputFiles.join(" ") & " -o " & outputFile
 
   if config.auxOutput:
-    stdout.styledWrite(styleBright, "Found " & inputFiles.len().intToStr() & " C files!\nCompiling...\n", resetStyle, styleDim, "$ " & command & "\n", resetStyle)
+    stdout.styledWrite(styleBright, "Found " & intToStr(inputFiles.len()) & " C files!\nCompiling...\n", resetStyle, styleDim, "$ " & command & "\n\n", resetStyle)
   
-  discard execShellCmd(command)
-  quit(0)
+  let exitCode = execShellCmd(command)
+  quit(exitCode)
--- src/initialize.nim
+++ src/initialize.nim
@@ -1,4 +1,4 @@
-import os
+import os, terminal
 
 const YAML_CONTENT = """--- # CLICK CONFIGURATION # ---
 
@@ -15,16 +15,21 @@ const YAML_CONTENT = """--- # CLICK CONFIGURATION # ---
 # default settings.              #
 ##################################
 
-compiler : clang    # What compiler click should use. `clang` by default.
-outputDir: ./build  # Directory to which the executable should be outputted. `./build` by default.
-auxOutput: true     # Whether Click should output auxiliary info: `Found <x> C files!` and `Runnning <command>`
+compiler   : clang       # What compiler Click should use. `clang` by default.
+outputDir  : ./build     # Directory to which the executable should be outputted. `./build` by default.
+auxOutput  : true        # Whether Click should output auxiliary info: `Found <x> C files!` and `Runnning <command>`
+ignoreDirs : [".vscode"] # List of directories to ignore in compilation.
+ignoreFiles: []          # List of paths to files to ignore in compilation.
 """
 
 
 ## Initialize the Click configuartion (by creating a new
 ## `click.yaml` file) or reinitialize if there's one already.
+## Simpler than you would think.
 proc initialize*(reinitialize: bool) =
   if reinitialize:
     removeFile("click.yaml")
 
+  stdout.styledWriteLine(fgCyan, if reinitialize: "Reinitialized `click.yaml`!" else: "Generated `click.yaml`!")
   writeFile("click.yaml", YAML_CONTENT)
+    
