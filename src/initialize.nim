e9ec5e0368011ae1b802765625c210e331d41753
aachh <antoni.chmiela@gmail.com>
2020-09-25 10:37:49 +0200

    new click.yaml template and revised comments for initialize()

--- src/initialize.nim
+++ src/initialize.nim
@@ -1,31 +1,33 @@
+
 import os, terminal
 
 const YAML_CONTENT* = """--- # CLICK CONFIGURATION # ---
 
-##################################/
+###################################
 # This file contains your Click   # 
 # configuration.                  #
 # To reinitialize, run            #
-# --------                        #
+# ---------                       #
 #    $ click init                 #
-# --------                        #
+# ---------                       #
 # again.                          #
 # This file is required for Click #
 # to run.                         #
 ##################################/
 
-name       : bin         # The name of the project. Your binary will be named this. `bin` by default.
-compiler   : clang       # What compiler Click should use. `clang` by default.
-outputDir  : ./build     # Directory to which the executable should be outputted. `./build` by default.
-auxOutput  : true        # Whether Click should output auxiliary info: `Found <x> C files!` and `Runnning <command>`
-ignoreDirs : [".vscode"] # List of directories to ignore in compilation.
-ignoreFiles: []          # List of paths to files to ignore in compilation.
+name       : "bin"       # The name of the project, what to name the binary. "bin" by default.
+compiler   : "clang"     # What compiler to use. "clang" by default.
+outputDir  : ./build     # To which directory to output the executable. `./build` by default.
+auxOutput  : true        # Whether Click should output auxiliary info: "Found <n> C files!" and "Runnning <command>..."
+ignoreDirs : [".vscode"] # List of directories to ignore in compilation. [".vscode"] by default.
+ignoreFiles: []          # List of paths to files to ignore in compilation. Empty by default.
+flags      : ["-Wall"]   # List of flags with which to compile. ["-Wall"] by default.
 """
 
 
 ## Initialize the Click configuartion (by creating a new
 ## `click.yaml` file) or reinitialize if there's one already.
-## Simpler than you would think. (Ran *only* with `click init`.)
+## Simpler than you would think.
 proc initialize*(reinitialize: bool, workspace: string) =
   if reinitialize:
     removeFile(workspace / "click.yaml")
